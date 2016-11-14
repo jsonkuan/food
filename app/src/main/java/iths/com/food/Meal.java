3e13b7dfa8d54daf4c9390272c3c18be1f60bbb2
Jason Kuan <jsonkuan@gmail.com>
2016-11-14 13:57:27 +0100

    Include initial file structure

---
+++ app/src/main/java/iths/com/food/Meal.java
@@ -0,0 +1,10 @@
+package iths.com.food;
+
+/**
+ * Created by jas0n on 2016-11-14.
+ */
+
+public class Meal {
+
+    //TODO: Model class for a meal
+}
---
+++ app/src/main/java/iths/com/food/MealActivity.java
@@ -0,0 +1,15 @@
+package iths.com.food;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MealActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meal);
+    }
+
+    //TODO: Reusable view with editable and non-editable objects
+}
