6379004fe77915185879ea53576d0eb7019b90de
Sebastian Strus <sebastian.strus1@gmail.com>
2016-11-15 11:53:34 +0100

    Added toolbar to meal_category_activity

--- app/src/main/java/iths/com/food/MealCategoryActivity.java
+++ app/src/main/java/iths/com/food/MealCategoryActivity.java
@@ -1,13 +1,19 @@
 package iths.com.food;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
+
+import static android.support.v7.appcompat.R.styleable.MenuItem;
 
 public class MealCategoryActivity extends AppCompatActivity {
 
@@ -15,6 +21,9 @@ public class MealCategoryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meal_category);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
         String[] foodtypes = {"Popcorn", "Sausages", "Coffee", "Fish", "Pancakes", "Bacon", "Canned Food", "Baby Food"};
 
@@ -28,9 +37,32 @@ public class MealCategoryActivity extends AppCompatActivity {
                     @Override
                     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                         String foodtypes = String.valueOf(adapterView.getItemAtPosition(i));
-                        Toast.makeText(MealCategoryActivity.this, "" + foodtypes + "is nr " + (i+1)+  "on this!", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MealCategoryActivity.this, "" + foodtypes + "is nr " + (i + 1) + "on this!", Toast.LENGTH_SHORT).show();
                     }
                 }
         );
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.meal_category_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_category_item:
+                Intent intent = new Intent(this, NewCategoryActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
