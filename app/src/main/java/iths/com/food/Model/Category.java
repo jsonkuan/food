3253e7473118c86bdfa11d1d7a508ad0bd485ace
Hrisitijan <hristijan.bocevski@gmail.com>
2016-11-16 22:47:28 +0100

    Addad Category class

---
+++ app/src/main/java/iths/com/food/Model/Category.java
@@ -0,0 +1,43 @@
+package iths.com.food.Model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Hristijan on 2016-11-16.
+ */
+
+public class Category {
+    private String name;
+    private double averageScore;
+    private ArrayList<Meal> meals;
+
+    public Category(String name, ArrayList<Meal> meals) {
+        this.name = name;
+        this.meals = meals;
+
+        // calculate average score for all meals
+        calculateAverageScore();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getAverageScore() {
+        return averageScore;
+    }
+
+    public ArrayList<Meal> getMeals() {
+        return meals;
+    }
+
+    private void calculateAverageScore() {
+        double sum = 0;
+
+        for (Meal meal: meals) {
+            sum += meal.getAverageScore();
+        }
+
+        averageScore = sum / meals.size();
+    }
+}
