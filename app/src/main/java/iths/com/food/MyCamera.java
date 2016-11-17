e0ecce54198ef0ad7eb802744930b03714e13f23
ASA <asa.kwarnmark@gmail.com>
2016-11-17 12:54:08 +0100

    Just adding files

--- app/src/main/java/iths/com/food/MyCamera.java
+++ app/src/main/java/iths/com/food/MyCamera.java
@@ -1,8 +1,62 @@
 package iths.com.food;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import java.io.File;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
+
 /**
  * Created by asakwarnmark on 2016-11-15.
  */
 
 public class MyCamera {
+
+    public static final int CAMERA_REQUEST_CODE = 1;
+    private static final String TAG = "TAG";
+    private Uri photoFilePath;
+
+
+    public void takePhoto(Context context) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(context.getPackageManager()) != null) {
+            File photo = createFile();
+            photoFilePath = Uri.fromFile(photo);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoFilePath);
+            ((Activity) context).startActivityForResult(intent, CAMERA_REQUEST_CODE);
+        }
+    }
+
+    private File createFile() {
+        File dir = getDirectory();
+        File file = new File(dir, "iths" + (new Date().getTime()) + ".jpg");
+        return file;
+    }
+
+    private File getDirectory() {
+        File photoDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        File ithsDir = new File(photoDir, "ithsPics");
+        if(!ithsDir.exists()) {
+            if(!ithsDir.mkdirs()) {
+                Log.d(TAG, "Could not create directory" + ithsDir.toString());
+            }
+        }
+        return ithsDir;
+    }
+
+    public void savePhoto() {
+
+    }
+
+    public void createThumbnail() {
+
+    }
+
 }
