3d487c1c646c38e204922e26cdda01d0ad7dc0e2
Sebastian Strus <sebastian.strus1@gmail.com>
2016-12-12 15:01:54 +0100

    Intro text

--- app/src/main/java/iths/com/food/WelcomeActivity.java
+++ app/src/main/java/iths/com/food/WelcomeActivity.java
@@ -5,58 +5,42 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.AnimatorSet;
 import android.animation.ObjectAnimator;
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import iths.com.food.MainActivity;
-
+/**
+ * Created by sebastianstrus on 11/12/16. WelcomeActivity class represents a launch screen.
+ * When animation finish loads MainActivity automatically.
+ */
 
 public class WelcomeActivity extends AppCompatActivity {
 
     protected AlphaAnimation fadeIn;
 
     AnimatorSet mAnimationSet;
-    ImageView myView;
 
 
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
-    //private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
 
-
-        // set button invisible
-        /*myView = (ImageView) findViewById(R.id.start);
-        myView.setVisibility(View.INVISIBLE);*/
-
         //fade in
         TextView tvIntro = (TextView) findViewById(R.id.intro_text);
         fadeIn = new AlphaAnimation(0.0f , 1.0f );
-        tvIntro.startAnimation(fadeIn);
         fadeIn.setDuration(3000);
-        //fadeIn.setStartOffset(5000);
-
-
-        /*
-        // Intro sound
-        final MediaPlayer mySound = MediaPlayer.create(this, R.raw.electrical);
-        mySound.start();*/
-
-
-
+        tvIntro.startAnimation(fadeIn);
+        //fadeIn.setStartOffset(1000);
 
         // Logo animation
         final ImageView img = (ImageView) findViewById(R.id.imageView);
@@ -71,9 +55,6 @@ public class WelcomeActivity extends AppCompatActivity {
             }
             @Override
             public void onAnimationEnd(Animation animation) {
-
-                // set button visible
-                //myView.setVisibility(View.VISIBLE);
 
                 //ObjectAnimator fadeIn = ObjectAnimator.ofFloat(myView, "alpha", .3f, 1f);
                 ObjectAnimator fadeIn = ObjectAnimator.ofFloat(img, "alpha", .3f, 1f);
@@ -93,22 +74,17 @@ public class WelcomeActivity extends AppCompatActivity {
                 });
                 mAnimationSet.start();
 
-                // When animation finish (or onClick), load Main Activity
+                // When animation finish loads MainActivity
                 img.startAnimation(fadeOutAnimation);
-                //finish();
-                //startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
             @Override
             public void onAnimationRepeat(Animation animation) {
-
             }
         });
 
     }
 
-    public void goToMainActivity(View view) {
-        finish();
-        startActivity(new Intent(getApplicationContext(), MainActivity.class));
-    }
 
 }
